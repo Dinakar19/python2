37cdb4e089a5d2c07e4aff9e9ff61beb6d5211f6
Dinakar19 <55407860+Dinakar19@users.noreply.github.com>
2020-03-11 17:58:08 +0530

    Add files via upload

---
+++ basics/classprograms/operators/logical.py
@@ -0,0 +1,6 @@
+a=1
+b=10
+print("a and b is:",(a and b))
+print("a or b is:",(a or b))
+print("a not b is:",(not a))
+
---
+++ basics/classprograms/operators/simplecalc.py
@@ -0,0 +1,15 @@
+a=int(input("Enter first number"))
+b=int(input("Enter second number"))
+c=a+b
+d=a-b
+e=a/b
+f=a//b
+g=a*b
+h=a%b
+print(" addition is :",c)
+print("Subtraction is:",d)
+print("Division with fraction is:",e)
+print("Division without fraction is:",f)
+print("multiplication is:",g)
+print("modulo is:",h)
+
